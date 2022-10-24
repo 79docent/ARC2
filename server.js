5834bab3b3c09ea7ccefd91c3fa55965e3bb5067
JKwidzinski <jakub.kwidzinski@gmail.com>
2022-10-24 22:00:17 +0200

    going back...

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path')
 const bodyParser = require("body-parser")
 const https = require("https")
 const {Datastore} = require('@google-cloud/datastore');
-var request = require('request');
 
 const datastore = new Datastore();
 
@@ -13,40 +12,40 @@ var counter = 0;
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.route('/getweather').get(function(req, res) {
+
     const cityName = 'Warszawa'
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d248ede03a6ab01b39c2b33e5adc019c&units=metric`
-    let request = request({
-        method: 'GET',
-        uri: url,
-    },function(response){
-        response.on("data", function(data){
-            const jsondata = JSON.parse(data)
-            const temp = jsondata.main.temp
-            const des = jsondata.weather[0].description
+    setInterval(() => {
+        https.get(url, function(response){
+            response.on("data", function(data){
+                const jsondata = JSON.parse(data)
+                const temp = jsondata.main.temp
+                const des = jsondata.weather[0].description
+        
+                const kind = 'Entity';
+        
+                const name = `sampleentity1${counter}`;
+        
+                const entityKey = datastore.key([kind, name]);
+        
+                const entity = {
+                    key: entityKey,
+                    data: {
+                        city: cityName,
+                        temp: temp,
+                        des: des,
+                    },
+                };
+        
+                datastore.save(entity)
+        
+                console.log(`City: ${entity.data.city} Temp: ${entity.data.temp}`)
     
-            const kind = 'Entity';
-    
-            const name = `sampleentity1${counter}`;
-    
-            const entityKey = datastore.key([kind, name]);
-    
-            const entity = {
-                key: entityKey,
-                data: {
-                    city: cityName,
-                    temp: temp,
-                    des: des,
-                },
-            };
-    
-            datastore.save(entity)
-    
-            console.log(`City: ${entity.data.city} Temp: ${entity.data.temp}`)
-
-            counter++;
+                counter++;
+            });
         });
-    });
-    request.end()
+    }, 30000);
+    
 });
 
 // app.post('/', function(req,res) {
