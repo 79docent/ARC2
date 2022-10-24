ed369072b7d9e4df0d7680d93858e8b1d0103578
JKwidzinski <jakub.kwidzinski@gmail.com>
2022-10-24 22:06:30 +0200

    set interval for 5 min

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.route('/getweather').get(function(req, res) {
         
                 const kind = 'Entity';
         
-                const name = `sampleentity1${counter}`;
+                const name = `sampleentity${counter}`;
         
                 const entityKey = datastore.key([kind, name]);
         
@@ -44,56 +44,9 @@ app.route('/getweather').get(function(req, res) {
                 counter++;
             });
         });
-    }, 30000);
+    }, 300000);
     
 });
-
-// app.post('/', function(req,res) {
-//     let counter = 0;
-//     let interval = setInterval(() => {
-//         const cityName = req.body.cityName
-//         const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d248ede03a6ab01b39c2b33e5adc019c&units=metric`
-        
-//         https.get(url, function(response){
-//             response.on("data", function(data){
-//                 const jsondata = JSON.parse(data)
-//                 const temp = jsondata.main.temp
-//                 const des = jsondata.weather[0].description
-    
-//                 const kind = 'Entity';
-    
-//                 const name = `sampleentity1${counter}`;
-    
-//                 const entityKey = datastore.key([kind, name]);
-    
-//                 const entity = {
-//                     key: entityKey,
-//                     data: {
-//                         city: cityName,
-//                         temp: temp,
-//                         des: des,
-//                     },
-//                 };
-    
-//                 datastore.save(entity)
-    
-//                 console.log(`City: ${entity.data.city} Temp: ${entity.data.temp}`)
-                
-//                 counter++;
-
-//                 cityName = 'Gdynia'
-
-//                 if(counter >= 3){
-//                     clearInterval(interval)
-//                 }
-
-//                 res.write("<h1>The temperature in " + cityName + " is " + temp + " degress Cel. </h1>");
-//                 res.write("<p>The weather description: " + des + "</p>");
-//                 res.send();
-//             })
-//         })
-//     }, 10000)
-// });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, _ => {
