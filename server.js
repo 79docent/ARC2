c7539f681950b1f996a478d6098773d4e1de5bd2
JKwidzinski <jakub.kwidzinski@gmail.com>
2022-10-24 19:34:41 +0200

    datastore trial

--- server.js
+++ server.js
@@ -3,30 +3,38 @@ const app = express()
 const path = require('path')
 const bodyParser = require("body-parser")
 const https = require("https")
+const {Datastore} = require('@google-cloud/datastore');
+
+const datastore = new Datastore();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-
 app.get('/updateWeather', function(req, res) {
     res.sendFile(path.join(__dirname) + '/views/index.html');
+    
 });
 
 app.post('/', function(req,res) {
     const cityName = req.body.cityName
     const url = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName+"&appid=d248ede03a6ab01b39c2b33e5adc019c&units=metric"
     
-
     https.get(url, function(response){
-        response.on("data", function(data){
+        response.on("data", async function(data){
             const jsondata = JSON.parse(data)
             const temp = jsondata.main.temp
             const des = jsondata.weather[0].description
 
+            const entity = {
+                city: cityName,
+                temp: temp,
+                des: des,
+            };
+
+            await datastore.save(entity)
+
             res.write("<h1>The temperature in " + cityName + " is " + temp + " degress Cel. </h1>");
             res.write("<p>The weather description: " + des + "</p>");
             res.send();
-            
         })
     })
 });
