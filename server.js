520e235d36e10237374db68f7df1aa9d4c3b343a
Jakub Kwidziński <s21022@pjwstk.edu.pl>
2022-11-08 15:45:47 +0100

    big changes

--- server.js
+++ server.js
@@ -4,48 +4,53 @@ const path = require('path')
 const bodyParser = require("body-parser")
 const https = require("https")
 const {Datastore} = require('@google-cloud/datastore');
+const cron = require("node-cron");
 
 const datastore = new Datastore();
-
-var counter = 0;
+const tasks = new Array();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.route('/getweather').get(function(req, res) {
-
-    const cityName = 'Warszawa'
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d248ede03a6ab01b39c2b33e5adc019c&units=metric`
-    setInterval(() => {
+function get_weather(user, cityName){
+    let task = cron.schedule("*/5 * * * *", () => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d248ede03a6ab01b39c2b33e5adc019c&units=metric`
         https.get(url, function(response){
             response.on("data", function(data){
                 const jsondata = JSON.parse(data)
                 const temp = jsondata.main.temp
                 const des = jsondata.weather[0].description
-        
-                const kind = 'Entity';
-        
-                const name = `sampleentity${counter}`;
-        
-                const entityKey = datastore.key([kind, name]);
-        
+            
+                const entityKey = datastore.key('Weather')
+            
                 const entity = {
                     key: entityKey,
                     data: {
+                        user: user,
                         city: cityName,
                         temp: temp,
                         des: des,
                     },
                 };
-        
+            
                 datastore.save(entity)
-        
+            
                 console.log(`City: ${entity.data.city} Temp: ${entity.data.temp}`)
-    
-                counter++;
             });
         });
-    }, 1*60*1000);
-    
+    });
+    task.start();
+    let taskObject = {
+        "task": task,
+        "user": user,
+        "city": cityName        
+    }
+    tasks.push(taskObject)
+}
+
+app.route('/getweather/:user/:city').get(function(req, res) {
+    let user = req.params.user
+    let cityName = req.params.city
+    get_weather(user, cityName);
 });
 
 const PORT = process.env.PORT || 8080;
